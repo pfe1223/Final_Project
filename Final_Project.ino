986b5c23ded936cce5ff57c6601123f062d43b51
Patrick <patrick.ester@gmail.com>
2014-11-22 17:09:13 -0600

    Initial Dump
    First code dump for the Arduino portion of Fashioning Circuits final
    project.

---
+++ Final_Project.ino
@@ -0,0 +1,46 @@
+//initialize variables to measure x-coordinate
+int xPin = 5;
+int xCoord1 = 0;
+int xCoord2 = 0;
+int xDiff = 0;
+
+//initialize variables to measure y-coordinate
+int yPin = 6;
+int yCoord1 = 0;
+int yCoord2 = 0;
+int yDiff = 0;
+
+//initialize variable to record z-coordinate
+int zCoord = 0;
+
+//threshold to record data or not
+int threshold = 20;
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(xPin, INPUT);
+}
+
+void loop(){
+  //record original x and y-coordinates
+  xCoord1 = analogRead(xPin);
+  yCoord1 = analogRead(yPin);
+  
+  delay(50); //short delay to account for movement
+  
+  //record second x and y-coordinates
+  xCoord2 = analogRead(xPin);
+  yCoord2 = analogRead(yPin);
+  
+  //calculate differences in x and y movements
+  xDiff = abs(xCoord2 - xCoord1);
+  yDiff = abs(yCoord2 - yCoord1);
+  if(xDiff > threshold && yDiff > threshold){
+    Serial.print("X = ");
+    Serial.println(xDiff);
+    Serial.print("Y = ");
+    Serial.println(yDiff);
+    Serial.println();
+  }
+  delay(500);
+}
